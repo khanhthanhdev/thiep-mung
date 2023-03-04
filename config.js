da2bddd0d6bd20cc7efed6336f5112c7e64799c0
Khanh Thanh <100078354+khanhthanhdev@users.noreply.github.com>
2023-03-04 08:13:13 +0700

    update image
    image women 10a1

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ const contentCard = 'Chúc các bạn nữ 10A1 ngày 8/3 tràn ngập niềm vu
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'hot-girl.png';
+const giftImage = 'w10.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
